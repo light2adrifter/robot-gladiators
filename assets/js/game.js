3a68a4577846ca1e4e4a588405115ad462edece4
light <light2adrifter@yahoo.com>
2021-11-01 11:34:33 -0400

    fixed invalid name

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,9 +154,20 @@ var randomNumber = function(min, max) {
 
   return value;
 };
+
+// function to set name
+var getPlayerName = function () {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?")
+  }
+  console.log("Tour robot's name is " + name);
+  return name;
+  
+};
 // creates players name as a variable via prompt input
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
